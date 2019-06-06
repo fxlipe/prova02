ead93ef807d4d94fab9760181d07f15f8e1d9a77
fxlipe <47851889+fxlipe@users.noreply.github.com>
2019-06-06 08:49:37 -0300

    Update prog02.c

--- prog02.c
+++ prog02.c
@@ -1,10 +1,9 @@
-  #include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
-void main(){
+
+void main() {
     char str[10];
-    int contador;
     printf("Digite uma string de até 10 digitos : ");
-    scanf("%c",&str);
-    contador=string_count(str);
-    printf("A string possui %d letras\n",contador);
+    scanf("%[^\n]%*c", &str);
+    printf("A string possui %d letras\n", strlen(str));
 }
